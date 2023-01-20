461da735f0bde0e964b4aba3965a6f96a0fecd68
che1 <nachwon@gmail.com>
2023-01-21 01:16:14 +0900

    Add episodes list

--- lib/screens/webtoon_detail.dart
+++ lib/screens/webtoon_detail.dart
@@ -43,62 +43,109 @@ class _WebtoonDetailState extends State<WebtoonDetail> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+          child: Column(
             children: [
-              Hero(
-                tag: widget.id,
-                child: Container(
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          blurRadius: 10,
-                          offset: const Offset(5, 5),
-                          color: Colors.black.withOpacity(0.5),
-                        )
-                      ]),
-                  width: 250,
-                  child: Image.network(widget.thumbnail),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 10,
+                              offset: const Offset(5, 5),
+                              color: Colors.black.withOpacity(0.5),
+                            )
+                          ]),
+                      width: 250,
+                      child: Image.network(widget.thumbnail),
+                    ),
+                  ),
+                ],
               ),
+              const SizedBox(height: 30),
+              FutureBuilder(
+                future: webtoon,
+                builder: ((context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(fontSize: 16),
+                          textAlign: TextAlign.justify,
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          '${snapshot.data!.genre}  /  ${snapshot.data!.age}',
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return const Text("...");
+                  }
+                }),
+              ),
+              const SizedBox(height: 30),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Container(
+                      child: Column(children: [
+                        for (var ep in snapshot.data!)
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(5),
+                              boxShadow: [
+                                BoxShadow(
+                                    blurRadius: 5,
+                                    color: Colors.black.withOpacity(0.1),
+                                    offset: const Offset(1, 1))
+                              ],
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 15,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    ep.title,
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  const Icon(Icons.chevron_right)
+                                ],
+                              ),
+                            ),
+                          )
+                      ]),
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              )
             ],
           ),
-          const SizedBox(height: 30),
-          FutureBuilder(
-            future: webtoon,
-            builder: ((context, snapshot) {
-              if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        snapshot.data!.about,
-                        style: const TextStyle(fontSize: 16),
-                        textAlign: TextAlign.justify,
-                      ),
-                      const SizedBox(height: 15),
-                      Text(
-                        '${snapshot.data!.genre}  /  ${snapshot.data!.age}',
-                        style: const TextStyle(fontSize: 16),
-                      ),
-                    ],
-                  ),
-                );
-              } else {
-                return const Text("...");
-              }
-            }),
-          )
-        ],
+        ),
       ),
     );
   }
