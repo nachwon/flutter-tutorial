5d3abe4b73ed14701e2a681180e24f1045e14aa4
che1 <nachwon@gmail.com>
2023-01-21 00:20:54 +0900

    Add more api calls

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:my_app/models/webtoon_detail_model.dart';
+import 'package:my_app/models/webtoon_episode_model.dart';
 import 'package:my_app/models/webtoon_model.dart';
 
 class ApiService {
@@ -18,6 +20,31 @@ class ApiService {
         webtoonInstances.add(webtoonModel);
       }
       return webtoonInstances;
+    } else {
+      throw Error();
+    }
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final response = await http.get(Uri.parse('$_baseUrl/$id'));
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    } else {
+      throw Error();
+    }
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getEpisodesById(String id) async {
+    final response = await http.get(Uri.parse('$_baseUrl/$id/episodes'));
+    if (response.statusCode == 200) {
+      List<WebtoonEpisodeModel> episodeInstances = [];
+      final List<dynamic> episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        final webtoonModel = WebtoonEpisodeModel.fromJson(episode);
+        episodeInstances.add(webtoonModel);
+      }
+      return episodeInstances;
     } else {
       throw Error();
     }
