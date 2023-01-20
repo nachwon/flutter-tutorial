1a92dc9ba3eea24fbed877186770d7ff72b8f850
che1 <nachwon@gmail.com>
2023-01-20 22:43:42 +0900

    Add api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,14 @@
+import 'package:http/http.dart' as http;
+
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev/';
+  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = "today";
 
-  void getTodaysToons() {}
+  void getTodaysToons() async {
+    final response = await http.get(Uri.parse('$baseUrl/$today'));
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      print(response.body);
+    }
+  }
 }
